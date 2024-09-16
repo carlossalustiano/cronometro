acfe0157eb24428d348d8c80e16cff8019de298f
Carlos <carloseduardo17488@gmail.com>
2024-09-16 00:42:08 -0300

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ function relogio(){
   
     if (el.classList.contains('iniciar')) {
       relogio.classList.remove('pausado');
+      relogio.style.transition = '0.3s';
       clearInterval(timer);
       iniciaRelogio();
     }
@@ -39,4 +40,29 @@ function relogio(){
   })
 }
 
+function botoes() {
+  const iniciar = document.querySelector('.iniciar');
+  const pausar = document.querySelector('.pausar');
+  const zerar = document.querySelector('.zerar');
+  
+  iniciar.addEventListener('click', (event) => {
+    pausar.style.display = 'block';
+    zerar.style.display = 'block';
+    
+    pausar.addEventListener('click', (event) => {
+      iniciar.innerText = 'Continue';
+      iniciar.addEventListener('click', (event) => {
+        iniciar.innerText = 'Start';
+      })
+    })
+  
+    zerar.addEventListener('click', (event) => {
+      zerar.style.display = 'none';
+      pausar.style.display = 'none';
+      iniciar.innerText = 'Start';
+    })
+  })
+}
+
+botoes();
 relogio();
